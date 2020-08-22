126793223fdcec475a01f2b78b85496022ca6377
Pedro Casis <pekasios@gmail.com>
2020-08-22 12:16:01 +0100

    Read a String and get Bing result page

--- src/main/java/com/pekasios/WebCrawler.java
+++ src/main/java/com/pekasios/WebCrawler.java
@@ -1,14 +1,38 @@
 package com.pekasios;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Scanner;
 
 class WebCrawler {
 
-    void readStringFromStdin() {
+    String readStringFromStdin() {
         Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        scanner.close();
+        return input;
+    }
 
-        while (scanner.hasNext()) {
-            System.out.print(scanner.next());
+    String searchResultsInBing(String term) {
+        StringBuilder output = new StringBuilder();
+        try {
+            URL bing = new URL("http://www.bing.com/search?q=" + term);
+
+            URLConnection yc = bing.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    yc.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                output.append(inputLine);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return output.toString();
     }
 }
--- src/test/java/com/pekasios/WebCrawlerTest.java
+++ src/test/java/com/pekasios/WebCrawlerTest.java
@@ -1,27 +1,40 @@
 package com.pekasios;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import static org.junit.Assert.*;
+
 public class WebCrawlerTest {
 
-    @Test void readStringFromStdin() {
-        String input = "searchTerm";
-        String expected = "searchTerm";
+    private WebCrawler webCrawler;
 
+    @BeforeEach
+    void setUp() {
+        webCrawler = new WebCrawler();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"searchTerm"})
+    void readStringFromStdin(String input) {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(baos);
-        System.setOut(ps);
 
-        WebCrawler webCrawler = new WebCrawler();
-        webCrawler.readStringFromStdin();
+        assertEquals("searchTerm", webCrawler.readStringFromStdin());
+    }
 
-        Assert.assertEquals(expected, baos.toString());
+    @ParameterizedTest
+    @CsvSource({"weather,<title>weather - Bing</title>"})
+    void getBingResultsFromSearchTerm(String input, String expected) {
+
+        assertTrue("Get a Bing result page for the search term",
+                webCrawler.searchResultsInBing(input).contains(expected));
     }
 
 }
